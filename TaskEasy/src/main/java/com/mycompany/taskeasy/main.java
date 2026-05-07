957aecf2ac8fec98cc4bb9529a3379a52aba5383
Roushinooll <daniel.silva23@estudante.ifms.edu.br>
2026-05-07 11:35:20 -0400

    Adicionado Menu Inicial

---
+++ TaskEasy/src/main/java/com/mycompany/taskeasy/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taskeasy;
+
+/**
+ *
+ * @author aluno
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao sistema taskeasy");
+        System.out.println("Cadastrar Tarefa");
+        System.out.println("Listar TarefaSS");
+    }
+    
+}
